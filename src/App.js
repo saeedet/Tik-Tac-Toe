2c7d3355310986ce6957a369ce38576e03a762d3
Saeed <saeed_et90@yahoo.com>
2021-10-06 18:08:25 +1100

    starting point

--- src/App.js
+++ src/App.js
@@ -1,23 +1,17 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from "react";
+import "./App.css";
+import "animate.css";
 
 function App() {
+  const [classes, setClasses] = useState(false);
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div
+        className={`kk  ${classes && " animate__animated animate__bounce"}`}
+        onClick={() => setClasses(true)}
+      >
+        kkkk
+      </div>
     </div>
   );
 }
